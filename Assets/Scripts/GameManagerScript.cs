27c349492cd4ac19c90e75a08b4632ad403291aa
PERLOFF-SP3\Noah <naperloff@hotmail.com>
2018-02-22 09:51:07 -0500

    Fix?

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    public static GameManagerScript instance;
+    public string uText;
+    public float count;
+    public float timer = 2;
+    public bool gameOver;
+
+	void Start ()
+    {
+        count = Random.Range(2.4f, 3.2f);
+        instance = this;
+	}
+	
+	void Update ()
+    {
+        if (!gameOver)
+        {
+            count -= Time.deltaTime;
+            if (count <= 0)
+            {
+                uText = "HOLD ON!";
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    uText = "";
+                    timer = 2f;
+                    count = Random.Range(2.4f, 3.2f);
+                    if (PlayerScript.instance.inPoleRange && PlayerScript.instance.holding)
+                    {
+                        gameOver = false;
+                    }
+                    else
+                    {
+                        gameOver = true;
+                    }
+                }
+            }
+         }
+            if (gameOver)
+            {
+                uText = "GAME OVER!\nPRESS R TO RESTART";
+                if (Input.GetKey(KeyCode.R))
+                {
+                    gameOver = false;
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("Prototype");
+                }
+            }             
+        }
+	}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float vel;
+    Rigidbody rb;
+    public bool holding;
+    public bool inPoleRange;
+    public bool jumping;
+    public Material DMat;
+    public Material SMat;
+    public Material FMat;
+    public static PlayerScript instance;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        instance = this;
+	}	
+
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            holding = true;
+        }
+        else
+        {
+            holding = false;
+        }
+        if (inPoleRange && holding)
+        {
+            gameObject.GetComponent<Renderer>().material = SMat;
+        }
+        else if (holding && !inPoleRange)
+        {
+            gameObject.GetComponent<Renderer>().material = FMat;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material = DMat;
+        }
+        if (!GameManagerScript.instance.gameOver)
+        {
+            Movement();
+            transform.position += transform.forward * vel / 3.5f;
+        }
+        Camera.main.transform.position = new Vector3(.58f, 3.4f, rb.transform.position.z - 2);
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W) && !holding)
+        {
+            vel += .002f;
+        }
+        if (Input.GetKey(KeyCode.S) && !holding)
+        {
+            vel -= .002f;
+        }
+        if (vel < 0 && (!Input.GetKey(KeyCode.S) || holding && !jumping))
+        {
+            vel += .001f;
+        }
+        else if (vel > 0 && (!Input.GetKey(KeyCode.W) || holding && !jumping))
+        {
+            vel -= .001f;
+        }
+        else if (vel <= .002f && vel >= -.002f && ((!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) || holding))
+        {
+            vel *= 0;
+        }
+        if (vel >= .1f)
+        {
+            vel = .1f;
+        }
+        if (vel <= -.1f)
+        {
+            vel = -.1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+           transform.eulerAngles += new Vector3(0, -2, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           transform.eulerAngles += new Vector3(0, 2, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+        {
+
+            rb.AddForce(0, 3.8f, 0, ForceMode.Impulse);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Pole")
+        {
+            inPoleRange = true;
+        }
+
+        if(col.gameObject.tag == "Goal")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Win");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Pole")
+        {
+            inPoleRange = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "floor")
+        {
+            jumping = false;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "floor")
+        {
+            jumping = true;
+        }
+    }
+}
