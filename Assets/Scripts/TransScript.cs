593bb5aae38f05733c3a83281d7eec4766d33d25
PERLOFF-SP3\Noah <naperloff@hotmail.com>
2018-02-22 15:58:09 -0500

    Playtest version of game

---
+++ Assets/Scripts/TransScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Pole")
+        {
+            Renderer R = col.gameObject.GetComponent<Renderer>();
+            Color color = R.material.color;
+            color.a = .4f;
+            R.material.color = color;
+            Debug.Log("HIT!");
+        }
+
+        if (col.gameObject.tag == "Wall")
+        {
+            Renderer R = col.gameObject.GetComponent<Renderer>();
+            Color color = R.material.color;
+            color.a = .4f;
+            R.material.color = color;
+            Debug.Log("WallHit");
+        }
+    }
+}
